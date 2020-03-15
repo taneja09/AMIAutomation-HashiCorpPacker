1cb75805466009e2ea3afce7384f0f54b37460d8
taneja09 <taneja.di@northeastern.edu>
2020-03-15 14:48:43 -0400

    added script sh to configure cloudwatch agent

---
+++ configCloudWatchAgent.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+	-a fetch-config -m ec2 \
+	-c file:/opt/aws/amazon-cloudwatch-agent/etc/cloudwatch-config.json -s
